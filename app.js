1365b8cd21d2f1d0b9f92bee92442bbca780a68d
Edwin Choi <edwinc91@Edwin-Chois-MBP.fios-router.home>
2017-04-23 11:51:57 -0400

    saving before i implement materialize

---
+++ app.js
@@ -0,0 +1,5 @@
+$('.image1').on('click', function() {
+  $('.image1').toggleClass('float-image-right');
+  $('.image1-description').toggleClass('hidden');
+  // $('.image1-description').toggle('slow');
+})
